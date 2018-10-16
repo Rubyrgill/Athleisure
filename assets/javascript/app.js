84411fca15a4c5eddcd6f9a52b92df506544e2e1
RubyG <rubyrgill@hotmail.com>
2018-10-15 23:44:05 -0400

    added sliders, animation for text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,5 +3,7 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.carousel').carousel();
+    $('.slider').slider();
+    $('.collapsible').collapsible();
 });
 
