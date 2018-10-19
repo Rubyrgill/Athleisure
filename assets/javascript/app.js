c8bc612a2dfd5180fe3314f03876295bc269f5df
RubyG <rubyrgill@hotmail.com>
2018-10-19 14:50:31 -0400

    Added research to the index page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,5 +5,22 @@ $(document).ready(function () {
     $('.carousel').carousel();
     $('.slider').slider();
     $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('.tabs').tabs();
 });
 
+//On click, will scroll down
+$(".research").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#overview").offset().top
+    },
+        'slow');
+});
+
+//On click, will scroll down
+$("#about-me-button").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".about-content").offset().top
+    },
+        'slow');
+});
